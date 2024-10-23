3b4fe299d390aae9eb892505d32f8653ea646fae
Krishnabatte <chandu.batte@gmail.com>
2024-10-23 10:46:00 +0100

    Create variable.tf

---
+++ ec2-instances/variable.tf
@@ -0,0 +1,16 @@
+variable "ami_id" {
+  default = "ami-0866a3c8686eaeeba"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "vpc_id" {
+  type = string
+}
+variable "security_group_id" {
+  type = string
+}
+variable "public_subnet_id" {
+  type = string
+}
