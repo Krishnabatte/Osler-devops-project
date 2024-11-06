7336b1e54b6100e2a6153ebc0a6f253dbb6700ef
Krishnabatte <chandu.batte@gmail.com>
2024-11-06 22:41:53 +0000

    Update main.tf

--- modules/ec2-resources/main.tf
+++ modules/ec2-resources/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "jenkins_flask_instance" {
   ami           = var.ami_id  # amazon Linux 2 AMI
   instance_type = var.instance_type               # Free-tier eligible instance
    # Use the key pair created earlier
- # key_name = aws_key_pair.ec2_key.key_name
+   key_name = aws_key_pair.ec2_key.key_name
   #key_name = module.ec2_tls_key.key_name
   # Associate security group
   #vpc_security_group_ids = [aws_security_group.jenkins_flask_sg.id]
