592b7212275913531a7695af84ef6b5314665b39
Krishnabatte <chandu.batte@gmail.com>
2024-11-06 21:51:20 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,15 @@
 module "network-resources" {
-  source = "./network-resources"
+  source = "./modules/network-resources"
   vpc_id = module.network-resources.vpc_id
 }
 
 module "security-resources" {
-  source = "./security-resources"
+  source = "./modules/security-resources"
   vpc_id = module.network-resources.vpc_id
 }
 
 module "ec2-resources" {
-  source = "./ec2-resources"
-
+  source = "./modules/ec2-resources"
   vpc_id = module.network-resources.vpc_id
   security_group_id = module.security-resources.security_group_id
   public_subnet_id = module.network-resources.public_subnet_id
