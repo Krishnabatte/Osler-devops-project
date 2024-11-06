6687c5d1a599553f8dd920ce7e3172f574748307
Krishnabatte <chandu.batte@gmail.com>
2024-11-06 23:39:40 +0000

    Update main.tf

--- modules/tls-private-key/main.tf
+++ modules/tls-private-key/main.tf
@@ -7,11 +7,11 @@ rsa_bits = 4096
 
  #Create a Key Pair for SSH access
 resource "aws_key_pair" "ec2_key" {
-  key_name   = ec2_pem
+  key_name   = "ec2_pem"
  public_key = tls_private_key.rsa_4096.public_key_openssh
 }
 
 resource "local_file" "private_key" {
  content = tls_private_key.rsa_4096.private_key_pem
- filename = var.key_name
+ filename = aws_key_pair.ec2_key.key_name
 }
