af64ff75fe36c59ea1aae06687fecc2683e36e25
Krishnabatte <chandu.batte@gmail.com>
2024-11-06 22:57:27 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "network-resources" {
   source = "./modules/network-resources"
-  vpc_id = module.network-resources.default.id
+  vpc_id = module.network-resources.vpc_id
 }
 
 module "security-resources" {
   source = "./modules/security-resources"
-  vpc_id = module.network-resources.default.id
+  vpc_id = module.network-resources.vpc_id
 }
 
 module "tls-private-key" {
@@ -15,7 +15,7 @@ key_name = "my-ec2-instance-key"
 
 module "ec2-resources" {
   source = "./modules/ec2-resources"
-  vpc_id = module.network-resources.default.id
+  vpc_id = module.network-resources.vpc_id
   security_group_id = module.security-resources.jenkins_flask_sg.id
   public_subnet_id = module.network-resources.public_subnet_id
   #key_name = module.ec2_tls_key.key_name
