c6bb81345ad0fa0b41a139e852fcddf476ee99a7
Krishnabatte <chandu.batte@gmail.com>
2024-11-06 23:00:57 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ key_name = "my-ec2-instance-key"
 module "ec2-resources" {
   source = "./modules/ec2-resources"
   vpc_id = module.network-resources.vpc_id
-  security_group_id = module.security-resources.jenkins_flask_sg.id
+  vpc_security_group_id = module.security-resources.jenkins_flask_sg.id
   public_subnet_id = module.network-resources.public_subnet_id
   #key_name = module.ec2_tls_key.key_name
 }
