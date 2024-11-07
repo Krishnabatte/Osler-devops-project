6632940f89fcca1ffdf9d295eecf8e84481abc86
Krishnabatte <chandu.batte@gmail.com>
2024-11-07 08:34:23 +0000

    Update main.tf

--- modules/internet_gateway/main.tf
+++ modules/internet_gateway/main.tf
@@ -8,7 +8,7 @@ resource "aws_route_table" "public_route_table" {
   vpc_id = var.vpc_id
 
   route {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 
