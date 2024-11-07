dd0e72d8da72babd93d538337d6cd92a79a2e32e
Krishnabatte <chandu.batte@gmail.com>
2024-11-07 07:43:54 +0000

    Update varables.tf

--- modules/internet_gateway/varables.tf
+++ modules/internet_gateway/varables.tf
@@ -2,3 +2,8 @@ variable "vpc_id" {
   description = "The VPC ID where the Internet Gateway will be attached."
   type        = string
 }
+
+variable "public_subnet_id" {
+  description = "The ID of the public subnet"
+  type        = string
+}
