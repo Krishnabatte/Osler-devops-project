cfb1f6fea9518a320fd4b14c2a1badff05e8be11
Krishnabatte <chandu.batte@gmail.com>
2024-11-07 01:18:32 +0000

    Update and rename Internet_NAT_gatewy.tf to main.tf

--- internet_gateway/main.tf
+++ internet_gateway/main.tf
@@ -19,3 +19,8 @@ resource "aws_route_table_association" "public_subnet_assoc" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route_table.id
 }
+
+# Output the IGW ID
+output "internet_gateway_id" {
+  value = aws_internet_gateway.igw.id
+}
