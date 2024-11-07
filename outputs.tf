1ccc4ba6f2d567f389b2cb467362b6ea9263375a
Krishnabatte <chandu.batte@gmail.com>
2024-11-07 08:05:42 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -15,3 +15,9 @@ output "jenkins_url" {
   description = "Jenkins URL"
   value       = "http://${module.ec2-resources.public_ip}:8080"
 }
+
+output "public_subnet_id" {
+  value = module.subnets.public_subnet_id
+}
+
+
