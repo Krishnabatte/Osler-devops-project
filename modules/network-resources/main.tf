23500967cfd30c6e98b3d18129c9ab5ae8cdf779
Krishnabatte <chandu.batte@gmail.com>
2024-11-07 07:42:06 +0000

    Update main.tf

--- modules/network-resources/main.tf
+++ modules/network-resources/main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "default" {
 }
 
 resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.default.id
+  vpc_id     = var.vpc_id
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true
   tags = {
