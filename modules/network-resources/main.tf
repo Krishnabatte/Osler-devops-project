0473fdfbc84bb15d9b1e4e98e67ce5e0ba4a2d53
Krishnabatte <chandu.batte@gmail.com>
2024-11-07 08:21:37 +0000

    Update main.tf

--- modules/network-resources/main.tf
+++ modules/network-resources/main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "default" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "0.0.0.0/0"
 
   tags = {
     Name = "default_vpc"
@@ -8,7 +8,7 @@ resource "aws_vpc" "default" {
 
 resource "aws_subnet" "public" {
   vpc_id     = var.vpc_id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "00.0.0.0/0"
   map_public_ip_on_launch = true
   tags = {
     Name = "public_subnet"
